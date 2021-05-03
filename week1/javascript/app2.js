f2812ccfbde9734ba48eb1a5b9131f0d5238597f
hsiangfeng <loveyou520babytw@gmail.com>
2021-05-03 14:38:25 +0800

    post

---
+++ week1/javascript/app2.js
@@ -0,0 +1,105 @@
+const productInput = document.getElementById('title');
+const productOriginPrice = document.getElementById('origin_price');
+const productPrice = document.getElementById('price');
+const addBtn = document.getElementById('addProduct');
+const productList = document.getElementById('productList');
+const clearAllBtn = document.getElementById('clearAll');
+const countProduct = document.getElementById('productCount');
+
+let productData = []
+
+function addProduct() {
+  const newProduct = productInput.value.trim();
+  const newOriginPrice = parseInt(productOriginPrice.value);
+  const newPrice = parseInt(productPrice.value);
+
+  const timeStamp = Math.floor(Date.now());
+  if (newProduct !== '') {
+    productData.push({
+      id: timeStamp,
+      title: newProduct,
+      origin_price: newOriginPrice || 0,
+      price: newPrice || 0,
+      is_enabled: false,
+    })
+
+    renderPage(productData);
+    productInput.value = '';
+    productOriginPrice.value = '';
+    productPrice.value = '';
+  }
+}
+
+addBtn.addEventListener('click', addProduct);
+
+function removeProduct(id) {
+  let newIndex = 0;
+  productData.forEach((item, key) => {
+    if (id == item.id) {
+      newIndex = key;
+    }
+  })
+  productData.splice(newIndex, 1);
+
+  renderPage(productData);
+}
+
+function completeProduct(id) {
+  productData.forEach((item) => {
+    if (id == item.id) {
+      item.is_enabled = !item.is_enabled;
+    }
+  })
+
+  renderPage(productData);
+}
+
+function clearAllTask(e) {
+  e.preventDefault();
+  productData = [];
+
+  renderPage(productData);
+}
+
+clearAllBtn.addEventListener('click', clearAllTask);
+
+function doSomething(e) {
+  const action = e.target.dataset.action;
+  const id = e.target.dataset.id;
+  if (action === 'remove') {
+    removeProduct(id)
+  } else if (action === 'complete') {
+    completeProduct(id)
+  }
+}
+
+productList.addEventListener('click', doSomething);
+
+function renderPage(data) {
+  let str = '';
+  data.forEach((item) => {
+    str += `
+    <tr>
+      <td>${item.title}</td>
+      <td width="120">
+        ${item.origin_price}
+      </td>
+      <td width="120">
+        ${item.price}
+      </td>
+      <td width="100">
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" id="is_enabled" ${item.is_enabled? 'checked': ''} data-action="complete" data-id="${item.id}">
+          <label class="form-check-label" for="is_enabled">${item.is_enabled? '啟用' : '未啟用'}</label>
+        </div>
+      </td>
+      <td width="120">
+        <button type="button" class="btn btn-sm btn-danger move" data-action="remove" data-id="${item.id}"> 刪除 </button>
+      </td>
+    </tr>`;
+  })
+  productList.innerHTML = str;
+  countProduct.textContent = data.length;
+}
+
+renderPage(productData);
