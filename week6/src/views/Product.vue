9a099dc8cb0daa0803c4e1620dcd98ad4d8751d6
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2021-06-11 09:50:32 +0800

    week6

---
+++ week6/src/views/Product.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is 單一產品頁面</h1>
+    {{ product.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data() {
+    return {
+      product: {},
+    };
+  },
+  created() {
+    const { id } = this.$route.params;
+    const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/product/${id}`;
+    this.$http.get(url)
+      .then((response) => {
+        if(response.data.success) {
+          this.product = res.data.product;
+        } else {
+          alert(response.data.message);
+        }
+      });
+  },
+};
+</script>
